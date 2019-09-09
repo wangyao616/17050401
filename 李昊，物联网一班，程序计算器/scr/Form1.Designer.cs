bcb95da4e2606cc8af00e82b04ca399957b76ee1
lilililia <40029122+lilililia@users.noreply.github.com>
2019-09-09 15:59:12 +0800

    Add files via upload

---
+++ 李昊，物联网一班，程序计算器/scr/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(432, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(285, 110);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始计算16位";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 15);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(350, 38);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "年份";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 59);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(350, 38);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "指令数";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(432, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(285, 111);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "开始计算32位";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 103);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(350, 38);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "平均工资";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 20F);
+            this.label2.Location = new System.Drawing.Point(750, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 27);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "存储容量需求";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 20F);
+            this.label3.Location = new System.Drawing.Point(750, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 27);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "存储器价格";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 20F);
+            this.label1.Location = new System.Drawing.Point(750, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 27);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "成本";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 20F);
+            this.label4.Location = new System.Drawing.Point(985, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 27);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 20F);
+            this.label5.Location = new System.Drawing.Point(985, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 27);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 20F);
+            this.label6.Location = new System.Drawing.Point(985, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 27);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1436, 512);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ 李昊，物联网一班，程序计算器/scr/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Y, B, Z, W, G, M, P1, P2, P3;
+            string y, b, z, w, g;
+            y = textBox1.Text;
+            z = textBox3.Text;
+            g = textBox5.Text;
+
+            Y = System.Convert.ToDouble(y);
+            Z = System.Convert.ToDouble(z);
+           
+            G = System.Convert.ToDouble(g);
+
+
+            M = 4080 * Math.Exp(0.28 * (Y - 1960));
+            P1 = 0.048 * Math.Pow(0.72, (Y - 1974));
+            P3 = (M / (Z * 30)) * G + (P1 * M);
+
+
+
+            label4.Text = M.ToString();
+            label5.Text = P1.ToString();
+            label6.Text = P3.ToString();
+
+
+
+
+
+
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double Y, B, Z, W, G, M, P1, P2, P3;
+            string y, b, z, w, g;
+            y = textBox1.Text;
+            z = textBox3.Text;
+            g = textBox5.Text;
+
+            Y = System.Convert.ToDouble(y);
+            Z = System.Convert.ToDouble(z);
+            G = System.Convert.ToDouble(g);
+
+
+            M = 4080 * Math.Exp(0.28 * (Y - 1960));
+            P1 = 0.3 * Math.Pow(0.72, (Y - 1974));
+            P3 = (M / (Z * 30)) * G + (P1 * M);
+
+
+
+            label4.Text = M.ToString();
+            label5.Text = P1.ToString();
+            label6.Text = P3.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
